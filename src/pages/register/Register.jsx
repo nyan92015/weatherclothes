b2f0839a8d2153bd3f59d47ef89b6ca6f98c16cf
muizu <muizu0920555@protonmail.com>
2023-08-11 00:55:38 +0900

    sign up

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,75 +1,67 @@
 import axios from "axios";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
 export default function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
 
+    const handleSignUp = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/v1/register", {
+                username: username,
+                password: password
+            });
+
+        } catch (error) {
+            // エラーハンドリング
+            console.error("An error occurred:", error);
+        }
+    };
   return (
-    <>
+    <React.Fragment>
       <div className="title">WeatherClothes</div>
       <div className="login-wrap">
         <div className="login-html">
-          <input
-            id="tab-1"
-            type="radio"
-            name="tab"
-            className="sign-in"
-            defaultChecked
-          />
-          <label htmlFor="tab-1" className="tab">
-            Sign In
-          </label>
+          <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked />
+          <label htmlFor="tab-1" className="tab">Sign In</label>
           <input id="tab-2" type="radio" name="tab" className="sign-up" />
-          <label htmlFor="tab-2" className="tab">
-            Sign Up
-          </label>
+          <label htmlFor="tab-2" className="tab">Sign Up</label>
           <div className="login-form">
             <div className="sign-in-htm">
               <div className="group">
-                <label htmlFor="user" className="label">
-                  Username
-                </label>
-                <input
-                  id="user"
-                  type="text"
-                  className="input"
-                  value={username} // ユーザー名の値をstateと結びつける
-                  onChange={handleUsernameChange} // ユーザー名の変更をハンドルする関数を設定
+                <label htmlFor="user" className="label">Username</label>
+                <input 
+                    id="user" 
+                    type="text" 
+                    className="input"
+                    value={username} // ユーザー名の値をstateと結びつける 
+                    onChange={handleUsernameChange} // ユーザー名の変更をハンドルする関数を設定
                 />
               </div>
               <div className="group">
-                <label htmlFor="pass" className="label">
-                  Password
-                </label>
-                <input
-                  id="pass"
-                  type="password"
-                  className="input"
-                  data-type="password"
-                  value={password} // パスワードの値をstateと結びつける
-                  onChange={handlePasswordChange} // パスワードの変更をハンドルする関数を設定
+                <label htmlFor="pass" className="label">Password</label>
+                <input 
+                    id="pass" 
+                    type="password" 
+                    className="input" 
+                    data-type="password"
+                    value={password} // パスワードの値をstateと結びつける
+                    onChange={handlePasswordChange} // パスワードの変更をハンドルする関数を設定
                 />
               </div>
               <div className="group">
-                <input
-                  id="check"
-                  type="checkbox"
-                  className="check"
-                  defaultChecked
-                />
-                <label htmlFor="check">
-                  <span className="icon"></span> Keep me Signed in
-                </label>
+                <input id="check" type="checkbox" className="check" defaultChecked />
+                <label htmlFor="check"><span className="icon"></span> Keep me Signed in</label>
               </div>
               <div className="group">
                 <input type="submit" className="button" value="Sign In" />
@@ -81,35 +73,42 @@ export default function Register() {
             </div>
             <div className="sign-up-htm">
               <div className="group">
-                <label htmlFor="user" className="label">
-                  Username
-                </label>
-                <input id="user" type="text" className="input" />
-              </div>
-              <div className="group">
-                <label htmlFor="pass" className="label">
-                  Password
-                </label>
-                <input
-                  id="pass"
-                  type="password"
+                <label htmlFor="user" className="label">Username</label>
+                <input 
+                  id="user" 
+                  type="text" 
                   className="input"
-                  data-type="password"
+                  value={username} 
+                  onChange={handleUsernameChange} 
                 />
               </div>
               <div className="group">
-                <label htmlFor="pass" className="label">
-                  Repeat Password
-                </label>
-                <input
-                  id="pass"
-                  type="password"
-                  className="input"
-                  data-type="password"
+                <label htmlFor="pass" className="label">Password</label>
+                <input 
+                  id="pass" 
+                  type="password" 
+                  className="input" 
+                  data-type="password" 
+                  value={password} 
+                  onChange={handlePasswordChange}
                 />
               </div>
               <div className="group">
-                <input type="submit" className="button" value="Sign Up" />
+                <label htmlFor="pass" className="label">Repeat Password</label>
+                <input 
+                  id="pass" 
+                  type="password" 
+                  className="input" 
+                  data-type="password" 
+                />
+              </div>
+              <div className="group">
+                <input 
+                  type="submit" 
+                  className="button" 
+                  value="Sign Up" 
+                  onClick={handleSignUp}
+                />
               </div>
               <div className="hr"></div>
               <div className="foot-lnk">
@@ -120,6 +119,10 @@ export default function Register() {
         </div>
       </div>
       <img src="back.png" className="back" />
-    </>
+      <script src="script.js"></script>
+    </React.Fragment>
   );
 }
+
+      
+
